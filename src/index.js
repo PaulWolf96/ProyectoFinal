93421978635a7736eb30c34882b63c12b4520913
PaulWolf96 <pablolobo96@gmail.com>
2022-09-19 22:59:47 -0300

    add footer

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Navbar from './components/navbar';
+import Footer from './components/footer';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+    <>
     <Navbar />
+    <Footer />
+    </>
+    
 );
 
 
