7859be8ceeb9a366e1b70f4d85e2d199edafb271
DianaConstanza <dianacbulacio@gmail.com>
2022-10-01 00:24:04 -0300

    Carusel

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,12 +1,14 @@
+import React from 'react';
 import Navbar from '../../components/navbar';
 import Footer from '../../components/footer';
+import Home from '../../components/homeIndex';
 
 
 const Index = () => {
     return (
      <>
       <Navbar/>
-      <homeIndex/>
+      <Home/>
       <Footer/>
      </>
     )
